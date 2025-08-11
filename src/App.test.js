d0fb08acc3c74cf9b851d63bbfb5f25e7fe34788
him-bevolve <167058675+him-bevolve@users.noreply.github.com>
2025-08-11 16:49:07 +0530

    New test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders home link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/home/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders about link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/about/i);
   expect(linkElement).toBeInTheDocument();
 });
